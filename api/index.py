37b10e0c02c10964b01ad7cfb4f1f3d737192a66
keysan <ksandaru99@gmail.com>
2024-04-08 09:57:09 +0530

    updated file

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask Vercel Example - Hello World", 200
+    return "Flask Vercel Example App by Kanishka", 200
 
 
 @app.errorhandler(404)
